47834a94dbc32f282fe3a43fa4debeda97367c23
Topher C <topherbyron@duck.com>
2024-10-08 10:54:29 -0400

    beginning day 1  week 3

---
+++ week-3/class/day-2/BufferedReader/src/main/java/com/pluralsight/Reader.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+import java.io.*;
+
+public class Reader {
+    public static void main(String args[]) {
+        try {
+            // create a FileReader object connected to the File
+            FileReader fileReader = new FileReader("src/main/resources/poem.txt");
+            // create a BufferedReader to manage input stream
+            BufferedReader bufReader = new BufferedReader(fileReader);
+            String input;
+
+            while ((input = bufReader.readLine()) != null) {
+                System.out.println(input);
+            }
+
+
+            // close the stream and release the resources
+            bufReader.close();
+        } catch (IOException e) {
+            // display stack trace if there was an error
+            e.printStackTrace();
+        }
+    }
+}
---
+++ week-3/class/day-2/day-2-objectives.md
@@ -0,0 +1,33 @@
+What did you learn yesterday?
+
+1. try/catch - Handling Exceptions
+   1.  Exception.printStackTrace()
+       1.  Displays exception and stack trace
+2. FileInputStream
+   1. scanner.close()
+3. break; - stops block of code
+4. continue; - starts loop back over
+5. switch - comparing variables value with option(s)
+   1. default case
+   2. can't evaluate non-constants
+6. IO - input/output
+
+
+Today's Objectives
+
+1. IO
+2. BufferedReader
+3. Writing Files
+4. Date basics (stretch)
+
+Today's Agenda
+
+1. Recap
+2. Finish reading files with Scanner
+3. Break
+4. BufferedReader
+5. Lunch
+6. Writing Files
+7.  Break
+8.  Dates
+9.  Recap and Q&A
