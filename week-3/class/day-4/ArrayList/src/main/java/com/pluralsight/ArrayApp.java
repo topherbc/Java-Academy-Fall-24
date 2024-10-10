e347424d197b6ea078a9e33387c4896a2da14758
Topher C <topherbyron@duck.com>
2024-10-10 11:21:32 -0400

    morning day 4 week 3

--- week-3/class/day-4/ArrayList/src/main/java/com/pluralsight/ArrayApp.java
+++ week-3/class/day-4/ArrayList/src/main/java/com/pluralsight/ArrayApp.java
@@ -3,11 +3,29 @@ import java.util.*;
 
 public class ArrayApp {
     public static void main(String[] args) {
+
+//        String[] sesameStreetCharacters = {"Big Bird", "Elmo", "Cookie Monster", "Oscar"};
+//
+//        System.out.println(sesameStreetCharacters[2]);
+
         ArrayList<String> sesameStreetCharacters = new ArrayList<String>();
 
         sesameStreetCharacters.add("Big Bird");
+        sesameStreetCharacters.add("Elmo");
+        sesameStreetCharacters.add("Cookie Monster");
+        sesameStreetCharacters.add("Oscar");
+        sesameStreetCharacters.add("Mr. Snuffleupagus");
+        sesameStreetCharacters.add("The count");
+        sesameStreetCharacters.add("Ernie");
+        sesameStreetCharacters.add("Bert");
 
 
-        System.out.println(sesameStreetCharacters.get(3));
+        sesameStreetCharacters.set(3, "Oscar the Grouch");
+
+        sesameStreetCharacters.remove(5);
+
+        Collections.sort(sesameStreetCharacters);
+
+        System.out.println(sesameStreetCharacters);
     }
 }
