dc5941338f928b9c87621f65ca1ebbb850f8cd4d
Topher C <topherbyron@duck.com>
2024-12-03 15:20:39 -0500

    basicdatasource

--- week-10/class/day-2/NorthwindTraders/src/main/java/com/pluralsight/App.java
+++ week-10/class/day-2/NorthwindTraders/src/main/java/com/pluralsight/App.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import java.sql.*;
 
 public class App {
@@ -15,47 +16,45 @@ public class App {
         String password = args[1];
 
         String query = "SELECT * FROM Products";
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet results = null;
-        try {
-            // Establishing connection
-            connection = DriverManager.getConnection(url, user, password);
-            statement = connection.prepareStatement(query);
 
-            // Executing query
-            results = statement.executeQuery();
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(user);
+        dataSource.setPassword(password);
 
-            // Processing the result set
-            System.out.println("Id   Name                             Price  Stock      \n---- -------------------------------- ------ ------ \n");
-            while (results.next()) {
-                // Replace with your column names and types
-                String id = formatSpaces(results.getString(1), 4);
-                String name = formatSpaces(results.getString("ProductName"), 32);
-                Double price = results.getDouble("UnitPrice");
-                String inStock = results.getString("UnitsInStock");
+        try (// Establishing connection
+             Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query);
+        ) {
+            try (// Executing query
+                 ResultSet results = statement.executeQuery()
+            ) {
+                // Processing the result set
+                System.out.println("Id   Name                             Price  Stock      \n---- -------------------------------- ------ ------ \n");
+                while (results.next()) {
+                    // Replace with your column names and types
+                    String id = formatSpaces(results.getString(1), 4);
+                    String name = formatSpaces(results.getString("ProductName"), 32);
+                    Double price = results.getDouble("UnitPrice");
+                    String inStock = results.getString("UnitsInStock");
 
-                //Stacked Info:
+                    //Stacked Info:
 //                System.out.printf("Product Id: %s \nName: %s \nPrice: %.2f \nStock: %s\n\n------------------\n\n", id, name, price, inStock);
-                //Row Info:
-//                String spaces = (id.length() == 2) ? "    " : (id.length() == 3) ? "   " : (id.length() == 4) ? "  " : " ";
-                System.out.printf("%s %s %.2f  %s  \n", id, name, price, inStock);
+                    //Row Info:
+                    System.out.printf("%s %s %.2f  %s  \n", id, name, price, inStock);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-            results.close();
-            statement.close();
-            connection.close();
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-
     }
 
     public static String formatSpaces(String v, int maxLength) {
         String newV = v;
-        for (int i = 0; i < maxLength-v.length(); i++) {
-            newV+=" ";
+        for (int i = 0; i < maxLength - v.length(); i++) {
+            newV += " ";
         }
         return newV;
     }
